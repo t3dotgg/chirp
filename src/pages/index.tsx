cbb01da7ec588aabd537381f56960716640aedbe
Theo Browne <me@t3.gg>
2023-03-14 17:25:33 -0700

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,7 +56,7 @@ const PostView = (props: PostWithUser) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
