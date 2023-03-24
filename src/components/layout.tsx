f29469fb3eee70fa236e3384b0b12794b219d507
Theo Browne <me@t3.gg>
2023-03-23 18:41:44 -0700

    Chirp blue

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,8 +2,8 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen justify-center">
-      <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
+    <main className="overflow-none flex h-screen justify-center">
+      <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
         {props.children}
       </div>
     </main>
