debd765e9240f9965ccd4f6001c24ddeb9e49a5b
huperniketes <huperniketes@gmail.com>
2023-04-13 15:34:15 -0400

    Update filterUserForClient() to fix null User.username.

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,9 +1,15 @@
-import type { User } from "@clerk/nextjs/dist/api";
-export const filterUserForClient = (user: User) => {
-  return {
+import { type User } from "@clerk/nextjs/dist/api";
+
+export function
+filterUserForClient(user: User)
+{
+  const u = {
     id: user.id,
-    username: user.username,
+    username: "",
     profileImageUrl: user.profileImageUrl,
-    externalUsername: user.externalAccounts.find((externalAccount) => externalAccount.provider === "oauth_github")?.username || null
   };
-};
+
+  if(user.username !== null)
+    u.username = user.username;
+  return  (u);
+}
